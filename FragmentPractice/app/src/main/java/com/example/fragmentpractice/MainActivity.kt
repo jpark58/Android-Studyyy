2d063917f9d807b9b816c06f7a82b7bef3d3c737
jpark58 <jinseopark94@gmail.com>
2021-05-25 03:27:33 +0900

    appbar study

--- FragmentPractice/app/src/main/java/com/example/fragmentpractice/MainActivity.kt
+++ FragmentPractice/app/src/main/java/com/example/fragmentpractice/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.fragmentpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.commit
 
 class MainActivity : AppCompatActivity() {
@@ -12,7 +14,20 @@ class MainActivity : AppCompatActivity() {
         if(savedInstanceState == null){
             supportFragmentManager.commit{
                 setReorderingAllowed(true)
-                add(R.id.fragment_container_view, ExampleFragment())
+                add(R.id.fragment_container_view, ExampleFragment(), "example1")
+                add(R.id.fragment_container_view, Example2Fragment(), "example2")
+            }
+        }
+
+        val tv = findViewById<TextView>(R.id.tv)
+        tv.setOnClickListener {
+            val example2Fragment = supportFragmentManager.findFragmentByTag("example2")
+            supportFragmentManager.commit {
+                if(example2Fragment != null){
+                    remove(example2Fragment)
+                }else{
+                    Toast.makeText(this@MainActivity, "더이상 존재하지 않아요!", Toast.LENGTH_SHORT).show()
+                }
             }
         }
     }
